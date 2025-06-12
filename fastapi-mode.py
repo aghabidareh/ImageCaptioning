5a330931104cbf734dc0745672c5001fcc1d524f
aghabidareh <aghabidareh@gmail.com>
2025-06-12 08:58:03 +0330

    define-app

--- fastapi-mode.py
+++ fastapi-mode.py
@@ -5,4 +5,10 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 from io import BytesIO
 from fastapi.responses import JSONResponse
 
+app = FastAPI(
+    title="Image Captioning App",
+    description="Image captioning app using ML science and python language",
+    version="1.0",
+)
+
 
